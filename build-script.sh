b6b7b537f84d2a3c72fe7709251b5672dc04a090
SafeEHA <eemarabs@gmail.com>
2024-01-20 13:40:35 +0100

    made edits on script file

--- build-script.sh
+++ build-script.sh
@@ -7,11 +7,12 @@ KEY_FILE="/Users/user/Downloads/bold-listener-320807.json"
 # Set the path to your source code and Dockerfile
 REPO_URL="https://github.com/SafeEHA/counter-app.git"
 git clone $REPO_URL
-cd /counter-app
+cd counter-app
 
 # Authenticate with Google Cloud
 gcloud auth activate-service-account --key-file="$KEY_FILE" 
-gcloud config set $PROJECT_ID
+gcloud config set project $PROJECT_ID
 
 # Trigger Cloud Build
-gcloud builds submit --config="cloudbuild.yml" "$DOCKERFILE_PATH" "$SOURCE_PATH"
+gcloud builds submit --config="cloudbuild.yml" 
+# "$DOCKERFILE_PATH" "$SOURCE_PATH"
