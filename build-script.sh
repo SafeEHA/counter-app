d48b7cb0df55a69fa8837242fd724ec5938b6b58
SafeEHA <eemarabs@gmail.com>
2024-01-23 18:11:36 +0100

    added steps to get artifact URL

--- build-script.sh
+++ build-script.sh
@@ -9,10 +9,10 @@ BUCKET_NAME="artifacts.safes-project.appspot.com"
 REPO_URL="https://github.com/SafeEHA/counter-app.git"
 
 # Clone the repository
-# git clone $REPO_URL
 cd counter-app
 
-# Authenticate with Google Cloud
+
+echo "Authenticating with Google Cloud..."
 gcloud auth activate-service-account --key-file=$KEY_FILE
 gcloud config set project $PROJECT_ID
 
@@ -25,14 +25,16 @@ LOCAL_FILE_PATH="."
 ZIP_FILE="counter-app.zip"
 BUILD_ID=$(gcloud builds list --limit=1 --format="value(ID)")
 
-# Remove any existing files in the GCS bucket directory
+
+echo "Removing existing files in the GCS bucket directory..."
 gsutil -m rm -r "gs://${BUCKET_NAME}/${BUCKET_DIRECTORY}/**"
 
-# Copy the source code to GCS
+echo "Copying source code to GCS..."
 gsutil cp "${ZIP_FILE}" "gs://${BUCKET_NAME}/${BUCKET_DIRECTORY}/"
 
-# Trigger Cloud Build
-curl -s -X POST https://cloudbuild.googleapis.com/v1/projects/safes-project/builds?alt=json \
+
+echo "Triggering Cloud Build..."
+BUILD_RESPONSE=$(curl -s -X POST https://cloudbuild.googleapis.com/v1/projects/safes-project/builds?alt=json \
   -H "Authorization: Bearer ${ACCESS_TOKEN}" \
   -H "Content-type: application/json" \
   -d '{
@@ -70,13 +72,20 @@ curl -s -X POST https://cloudbuild.googleapis.com/v1/projects/safes-project/buil
     ],
     "images": ["gcr.io/safes-project/my-image"]
 
- }' 
+ }')
+
+    if [[ $BUILD_RESPONSE == *"\"status\": \"ERROR\""* ]]; then
+    echo "Build failed. Check the Cloud Build logs for details."
+    exit 1
+    fi 
     # Get build information from Cloud Build API
     BUILD_INFO=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}", "https://cloudbuild.googleapis.com/v1/projects/${PROJECT_ID}/builds/${BUILD_ID}")
-
 
     # Extract artifact URL from the build results
     ARTIFACT_URL=$(echo "$BUILD_INFO" | jq ".artifacts.images[0]")
         
-    # Print the artifact URL
+    if [ -z "$ARTIFACT_URL" ]; then
+    echo "Error: No container artifact URL found. Check Cloud Build logs for details."
+    exit 1
+    fi    
     echo "Container Artifact URL: ${ARTIFACT_URL}"
